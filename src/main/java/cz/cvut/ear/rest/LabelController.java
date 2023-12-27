859f423d91c731e7381f9fcb148b3472f1c6f8a7
Dmytro Kovalov <kovaldmy@fel.cvut.cz>
2023-12-27 16:55:11 +0100

    add label controller

---
+++ src/main/java/cz/cvut/ear/rest/LabelController.java
@@ -0,0 +1,56 @@
+package cz.cvut.ear.rest;
+
+import cz.cvut.ear.helper.RestUtils;
+import cz.cvut.ear.model.Label;
+import cz.cvut.ear.service.LabelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@PreAuthorize("hasAnyRole('EMPOWERED_EMPLOYEE', 'REGULAR_EMPLOYEE')")
+@RequestMapping("/api/labels")
+public class LabelController {
+    @Autowired
+    private LabelService labelService;
+
+    // get label with concrete id
+    @GetMapping(value = "/{labelId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Label getLabelById(@PathVariable Long labelId) {
+        return labelService.getLabelById(labelId);
+    }
+
+    // get label with concrete name
+    @GetMapping(value = "/{labelName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Label getLabelByName(@PathVariable String labelName) {
+        return labelService.getLabelByName(labelName);
+    }
+
+    // create new label
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createLabel(@RequestBody Label label) {
+        labelService.createLabel(label);
+        final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/{id}", label.getId());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    // update the whole label (with cascade change in all dependencies)
+    @PreAuthorize("hasRole('EMPOWERED_EMPLOYEE')")
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateLabel(@RequestBody Label label) {
+        labelService.updateLabel(label);
+    }
+
+    // delete the whole label
+    @PreAuthorize("hasRole('EMPOWERED_EMPLOYEE')")
+    @DeleteMapping(value = "/{labelId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLabel(@PathVariable Long labelId) {
+        labelService.deleteLabel(labelId);
+    }
+}
