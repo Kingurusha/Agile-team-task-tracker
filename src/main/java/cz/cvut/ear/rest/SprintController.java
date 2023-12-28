7b2fef6c6ca57d18827e77dd7a850d7c3e1ba25f
Dmytro Kovalov <kovaldmy@fel.cvut.cz>
2023-12-28 16:28:05 +0100

    edit sprint controller

--- src/main/java/cz/cvut/ear/rest/SprintController.java
+++ src/main/java/cz/cvut/ear/rest/SprintController.java
@@ -1,20 +1,67 @@
 package cz.cvut.ear.rest;
 
+import cz.cvut.ear.helper.RestUtils;
+import cz.cvut.ear.model.Sprint;
 import cz.cvut.ear.service.SprintService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/rest/sprint")
+@PreAuthorize("hasAnyRole('EMPOWERED_EMPLOYEE', 'REGULAR_EMPLOYEE')")
+@RequestMapping("/api/sprints")
 public class SprintController {
-    private final SprintService sprintService;
-    private static final Logger LOG = LoggerFactory.getLogger(SprintController.class);
-
     @Autowired
-    public SprintController(SprintService sprintService) {
-        this.sprintService = sprintService;
+    private SprintService sprintService;
+
+    // get sprint with concrete id
+    @GetMapping(value = "/{sprintId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Sprint getSprintById(@PathVariable Long sprintId) {
+        // todo: validation
+        return sprintService.getSprintById(sprintId);
+    }
+
+
+    // create new sprint
+    @PreAuthorize("hasRole('EMPOWERED_EMPLOYEE')")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createSprint(@RequestBody Sprint sprint) {
+        // todo: validation
+        sprintService.createSprint(sprint);
+        final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/{id}", sprint.getId());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    // update the whole sprint
+    @PreAuthorize("hasRole('EMPOWERED_EMPLOYEE')")
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateSprint(@RequestBody Sprint sprint) {
+        // todo: validation
+        sprintService.updateSprint(sprint);
+    }
+
+    // update some parts of sprint
+    @PatchMapping(value = "/{sprintId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void partialSprintUpdate(@PathVariable Long sprintId,
+                                    @RequestBody Map<String, Object> updates) {
+        // todo: validation
+        sprintService.partialSprintUpdate(sprintId, updates);
+    }
+
+    // delete task from sprint
+    @PreAuthorize("hasRole('EMPOWERED_EMPLOYEE')")
+    @DeleteMapping(value = "/{sprintId}/tasks/{taskId}")
+    public void removeTaskFromSprint(@PathVariable Long sprintId,
+                                     @PathVariable Long taskId) {
+        // todo: validation
+        sprintService.removeTaskFromSprint(sprintId, taskId);
     }
 }
